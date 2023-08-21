b081e34578906bc33a3185961c7d386dbaba0f6a
Anil Kumar Musinada <121026687+Musinada@users.noreply.github.com>
2023-08-21 12:55:29 +0530

    Update jenkinsauto.sh

--- jenkinsauto.sh
+++ jenkinsauto.sh
@@ -7,7 +7,7 @@
 #
 # version: v1
 #
-# This job is to install all the required software’s for eks cluster 
+# This job is to setup Jenkins on vm 
 ###################################################
 set -x
 
@@ -18,3 +18,77 @@ sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv
 sudo unzip awscliv2.zip
 sudo ./aws/install
 /usr/local/bin/aws --version
+
+# Change Host Name to Jenkins
+sudo hostnamectl set-hostname Jenkins
+
+# Perform update first
+sudo apt update
+
+# Install Java 11
+sudo apt install default-jdk -y
+
+# Verify Java Version
+java -version
+
+# Maven Installation
+sudo apt install maven -y
+mvn --version
+
+# Jenkins Setup
+# Add Repository key to the system
+curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+
+# Append debian package repo address to the system
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+# Update Ubuntu package
+sudo apt update
+
+# Update Linux package
+# sudo yum update
+
+# Install Jenkins
+sudo apt install jenkins -y
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
